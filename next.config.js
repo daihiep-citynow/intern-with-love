2630f744621d12a7cbe28f673a8974241462c3cd
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-08-03 09:38:11 +0700

    -test: stylelint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
+const StylelintPlugin = require("stylelint-webpack-plugin"); // eslint-disable-line
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // eslint-disable-next-line
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
 };
 
 module.exports = nextConfig;
