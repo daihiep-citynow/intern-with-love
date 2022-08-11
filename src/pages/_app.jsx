90576fb05b7f9e5554cf47ee03c748ccf45efa38
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-08-11 15:25:49 +0700

    feat: Header component inside -complete

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,10 @@
+import axios from "axios";
+
 import { useLanguage } from "../hooks";
 import "antd/dist/antd.css";
 import "./index.css";
+
+axios.defaults.baseURL = "https://intern-with-love.daihiep.co/";
 
 const MyApp = ({ Component, pageProps }) => (
   <useLanguage.LanguageProvider>
