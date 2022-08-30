8632f2e528e5946872b0a11381f49af2d60760b2
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-08-30 17:42:23 +0700

    feat: test

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,4 +1,4 @@
-// libraries
+// libs
 import { useEffect, useState } from "react";
 import axios from "axios";
 
--- src/views/SettingScreen/index.jsx
+++ src/views/SettingScreen/index.jsx
@@ -6,5 +6,4 @@ const SettingScreen = () => (
     <span />
   </SettingLayout>
 );
-
 export default SettingScreen;
