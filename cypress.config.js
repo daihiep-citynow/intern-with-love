375262b431553cf82419c395f7476c94587006f5
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-09-06 13:30:48 +0700

    chore: config cucumber & folder e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,25 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  projectId: "i5x1t3",
   e2e: {
     // eslint-disable-next-line
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        }),
+      );
+
+      return config;
     },
+    baseUrl: "http://localhost:3000",
+    specPattern: "./cypress/**/*.{feature, mjs}",
   },
 });
